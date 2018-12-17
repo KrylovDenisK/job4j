839695623ddaa218c7753881cc6b4e30752ea89d
Denis Krylov <d.krylovk@gmail.com>
2018-12-17 23:15:29 +0700

    1.3.JavDoc.Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,16 @@
-package ru.job4j.calculate;
-
+﻿package ru.job4j.calculate;
+/**
+ * Calculate Класс для вывода Hellow World  
+ * @author krylov
+ * @since 17.12.2018
+ * @version 1
+*/
 public class Calculate{
+	/**
+	Вывод строки в консоль
+	@param args - args
+	
+	*/
 	public static void main(String[] args){
 		System.out.println("Hellow world");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+﻿/**
+* Package for calculate task.
+*
+* @author DenisKrylov
+* @version 1
+* @since 17.12.2018
+*/
+
