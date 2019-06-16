b0541d0993545e3863f8e13afcca9a3ed460c118
Denis Krylov <d.krylovk@gmail.com>
2019-06-16 23:26:32 +0700

    Реализовать SimpleArray<T>

---
+++ chapter_005/src/main/java/ru/job4j/iterator/SimpleArray.java
@@ -0,0 +1,73 @@
+package ru.job4j.iterator;
+
+
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.stream.IntStream;
+
+public class SimpleArray<T> implements Iterable<T> {
+    private T[] array;
+    private int index;
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<>() {
+            private int poz = 0;
+            @Override
+            public boolean hasNext() {
+                return poz < array.length;
+            }
+
+            @Override
+            public T next() {
+                return array[poz++];
+            }
+        };
+    }
+
+    public SimpleArray(T[] array) {
+        this.array = array;
+    }
+
+    /**
+     * Add T element
+     * @param model
+     */
+    public void add(T model) {
+        array[index++] = model;
+    }
+
+    /**
+     * Set item by index
+     * @param index new element
+     * @param model item
+     */
+    public void set(int index, T model) {
+        array[index] = model;
+    }
+
+    /**
+     *item deletion and rest
+     * @param index
+     */
+    public void remove(int index) {
+        if (!Objects.isNull(get(index))) {
+            if (index != array.length - 1) {
+                IntStream.range(index + 1, array.length).forEach(x -> array[x - 1] = array[x]);
+                array[array.length - 1] = null;
+            } else {
+                array[index] = null;
+            }
+            this.index--;
+        }
+    }
+
+    /**
+     * Get item by index
+     * @param index
+     * @return
+     */
+    public T get(int index) {
+        return array[index];
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/iterator/SimpleArrayTest.java
@@ -0,0 +1,64 @@
+package ru.job4j.iterator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class SimpleArrayTest {
+    private SimpleArray<Integer> simpleArray = new SimpleArray<>(new Integer[10]);
+    private Iterator it = simpleArray.iterator();
+
+    @Before
+    public void setUp() {
+        for (int i = 0; i < 10; i++) {
+            simpleArray.add(i);
+        }
+    }
+    @Test
+    public void whenAddModel() {
+        assertThat(simpleArray.get(5), is(5));
+        assertThat(simpleArray.get(9), is(9));
+    }
+
+    @Test
+    public void whenSetModel() {
+        simpleArray.set(5, 100);
+        assertThat(simpleArray.get(5), is(100));
+    }
+
+    @Test
+    public void whenRemoveModel() {
+        simpleArray.remove(5);
+        assertThat(simpleArray.get(5), is(6));
+        simpleArray.remove(6);
+        assertThat(simpleArray.get(6), is(8));
+    }
+    @Test
+    public void whenIteratorThenSequence() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(0));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(3));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(5));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(6));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(7));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(8));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(9));
+        assertThat(it.hasNext(), is(false));
+    }
+}
